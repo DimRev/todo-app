d12d355f158754fd0925df6185fd21ef66619bf4
DimRev <dimarev444@gmail.com>
2023-12-21 15:27:15 +0200

    Add: Added LoginSignup cmp to AppHeader and rough style

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,18 +1,28 @@
+import { LoginSignup } from './LoginSignup.jsx'
 import { UserMsg } from './UserMsg.jsx'
 
 const { Link, NavLink } = ReactRouterDOM
 const { useNavigate } = ReactRouter
 
 export function AppHeader() {
+
+  function onSetUser(user) {
+
+  }
+
   return (
     <header className="app-header">
-      <section className="main-logo"></section>
+      <section className="main-logo">
+        <h1>Todos</h1>
+      </section>
       <section className="main-nav-section">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/todo">Todos</NavLink>
         <NavLink to="/about">About</NavLink>
       </section>
-      <section className="main-login-section"></section>
+      <section className="main-login-section">
+        <LoginSignup onSetUser={onSetUser}/>
+      </section>
       <UserMsg />
     </header>
   )
