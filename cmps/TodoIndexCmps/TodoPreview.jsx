b371b2e6d5acff01cdb240641f1c1a05cfa53a6b
DimRev <dimarev444@gmail.com>
2023-12-22 09:26:55 +0200

    Add: Added Edit & Details links for each todo

--- cmps/TodoIndexCmps/TodoPreview.jsx
+++ cmps/TodoIndexCmps/TodoPreview.jsx
@@ -1,9 +1,13 @@
+const { Link, NavLink } = ReactRouterDOM
+
 export function TodoPreview({ onToggleTodo, todo }) {
   return (
     <React.Fragment>
       <button className="toggle-todo-btn" onClick={() => onToggleTodo(todo)}>
         {todo.isDone ? 'complete' : 'active'}
       </button>
+      <Link to={`/todo/${todo._id}`}><button>Edit</button></Link>
+      <Link to={`/todo/details/${todo._id}`}><button>Details</button></Link>
       <span className={todo.isDone ? 'complete' : 'active'}>{todo.todo}</span>
     </React.Fragment>
   )
